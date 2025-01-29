ba3c54d84b9eeb4a636a6c25cb3841b45cadb740
brunodelaine <bruno@mkey.fr>
2025-01-29 15:32:57 +0100

    🔨  change path js

---
+++ js/modules/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    DEFAULT_PROFILE_PHOTO: 'assets/images/hero2.jpg',
+    STORAGE_KEY: 'form_submissions',
+    MAX_SUBMISSIONS: 5,
+    SUBMISSION_TIMEOUT: 24 * 60 * 60 * 1000,
+    EMAIL_SERVICE_ID: 'service_aikb04n',
+    EMAIL_TEMPLATE_ID: 'template_fjeti6b'
+};
